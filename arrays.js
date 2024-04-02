54f5048de5f8e5d0b7963be790e526bf9708e70d
Buddy Agyin <20474715+agyin3@users.noreply.github.com>
2024-04-02 12:09:14 -0600

    initial commit

---
+++ arrays.js
@@ -0,0 +1,39 @@
+// To help us use arrays with real world problems we are going to simulate a used car dealer that has cars in their inventory.
+
+// The car dealer has all of their inventory housed in the array seen below.  Scroll down past the data to find out how you can help the car dealer.
+
+let inventory = [
+    "Corolla",
+    "Camry",
+    "Rav4",
+    "Echo",
+    "Sentra",
+    "Civic",
+    "Pinto",
+    "Range Rover",
+];
+
+// Example for loop:
+
+// arr = [1,2,3,4];
+// for (let i = 0; i < arr.length; i++) {
+//     arr[i]; // 1,2,3,4
+// }
+
+// ==== Challenge 1 ====
+// The dealer can't recall the information for a car with an index of 3 on his lot. Help the dealer find out which car is has an index of 3 and console log the following - `Car ${name} has an index of 3`
+
+// ==== Challenge 2 ====
+// The dealer needs the information on the last car in their inventory.  What is the name of the last car in the inventory?  Console log your answer
+
+// ==== Challenge 3 ====
+// The marketing team wants to add three more cars to the inventory. Add two car names to the end of the array and one car name to the start of the array.
+
+// ==== Challenge 4 ====
+// The marketing team wants to display the list of cars in reverse order. Create a new variable called inventoryReversed that is the reverse of the inventory array. Do not modify the inventory array
+
+// ==== Challenge 5 ====
+// The car lot manager needs to list out every car that they have an inventory for. Create a loop that will console log the following senctence for each car: `We currently have a {name} in our lot`
+
+// ==== Challenge 6 ====
+// Accounting would like a way to remove a car from the inventory. Create a function called removeCar that takes in two parameters: an array of cars and a car name. Your function should return a new array that no longer includes the car to be removed
